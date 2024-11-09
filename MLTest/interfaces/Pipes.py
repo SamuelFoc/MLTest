ec4eda3059ece6e037116f9f62d31a885725bdf0
SamuelFoc <samo.sipikal@gmail.com>
2024-11-09 15:45:57 +0100

    Update pipeline interface

--- MLTest/interfaces/Pipes.py
+++ MLTest/interfaces/Pipes.py
@@ -1,7 +1,31 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from MLTest.interfaces.Components import Component
+from MLTest.interfaces.Typing import DF
+from typing import List
 
-class Pipe(ABC):
+
+class Pipeline(ABC):
+    """
+    Base interface for all pipelines.
+    """
+    def __init__(self, components: List[Component]):
+        """
+        Initializes the pipeline with a list of components.
+
+        Parameters:
+        - components (List[Component]): A list of components to execute in the pipeline.
+        """
+        self.components = components
+
     @abstractmethod
-    def run(self) -> Any:
+    def run(self, *args) -> DF:
+        """
+        Executes the pipeline.
+
+        Parameters:
+        - args: The input arguments for the pipeline.
+
+        Returns:
+        - DF: The result of the pipeline (if applicable).
+        """
         pass
